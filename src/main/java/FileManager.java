d4dad43308fe2794279cf8a57c7700850c2d0423
maritoo17 <msancgim@myuax.com>
2024-05-03 11:37:47 +0200

    3

---
+++ src/main/java/FileManager.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class FileManager {
+    private Experimento experimento;
+
+    public FileManager(Experimento experimento) {
+        this.experimento = experimento;
+    }
+
+    public void abrirArchivo(String ruta) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta))) {
+            experimento = (Experimento) ois.readObject();
+        }
+    }
+
+    public void guardarArchivo(String ruta) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))) {
+            oos.writeObject(experimento);
+        }
+    }
+}
