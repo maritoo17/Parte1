8327173fbea601684cb28b8d3f44273ee314fa99
maritoo17 <msancgim@myuax.com>
2024-05-03 12:17:27 +0200

    8

--- src/main/java/Poblacion.java
+++ src/main/java/Poblacion.java
@@ -1,25 +1,36 @@
-public class Poblacion {
-    private String nombre;
-    private int cantidad;
+import java.io.Serializable;
+import java.util.Date;
 
-    public Poblacion(String nombre, int cantidad) {
+public class Poblacion implements Serializable {
+    private String nombre;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int bacteriasIniciales;
+    private double temperatura;
+    private String luminosidad;  // Alta, Media, Baja
+    private int comidaInicial;
+    private int diaIncremento;
+    private int comidaIncremento;
+    private int comidaFinal;
+
+    public Poblacion(String nombre, Date fechaInicio, Date fechaFin, int bacteriasIniciales,
+                     double temperatura, String luminosidad, int comidaInicial, int diaIncremento,
+                     int comidaIncremento, int comidaFinal) {
         this.nombre = nombre;
-        this.cantidad = cantidad;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.bacteriasIniciales = bacteriasIniciales;
+        this.temperatura = temperatura;
+        this.luminosidad = luminosidad;
+        this.comidaInicial = comidaInicial;
+        this.diaIncremento = diaIncremento;
+        this.comidaIncremento = comidaIncremento;
+        this.comidaFinal = comidaFinal;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    @Override
-    public String toString() {
-        return "Poblacion{" +
-                "nombre='" + nombre + '\'' +
-                ", cantidad=" + cantidad +
-                '}';
-    }
+    // Add getters for other fields if necessary
 }
