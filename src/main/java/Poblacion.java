026e95631cc36714708cb2ec58643e100e53021c
maritoo17 <msancgim@myuax.com>
2024-05-03 12:24:47 +0200

    12

--- src/main/java/Poblacion.java
+++ src/main/java/Poblacion.java
@@ -32,5 +32,81 @@ public class Poblacion implements Serializable {
         return nombre;
     }
 
-    // Add getters for other fields if necessary
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public int getBacteriasIniciales() {
+        return bacteriasIniciales;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public String getLuminosidad() {
+        return luminosidad;
+    }
+
+    public int getComidaInicial() {
+        return comidaInicial;
+    }
+
+    public int getDiaIncremento() {
+        return diaIncremento;
+    }
+
+    public int getComidaIncremento() {
+        return comidaIncremento;
+    }
+
+    public int getComidaFinal() {
+        return comidaFinal;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public void setBacteriasIniciales(int bacteriasIniciales) {
+        this.bacteriasIniciales = bacteriasIniciales;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public void setLuminosidad(String luminosidad) {
+        this.luminosidad = luminosidad;
+    }
+
+    public void setComidaInicial(int comidaInicial) {
+        this.comidaInicial = comidaInicial;
+    }
+
+    public void setDiaIncremento(int diaIncremento) {
+        this.diaIncremento = diaIncremento;
+    }
+
+    public void setComidaIncremento(int comidaIncremento) {
+        this.comidaIncremento = comidaIncremento;
+    }
+
+    public void setComidaFinal(int comidaFinal) {
+        this.comidaFinal = comidaFinal;
+    }
 }
+
